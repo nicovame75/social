5a15d11124d708f1faa256823def87554e9bbd91
ernesto <ernesto.tejeda87@gmail.com>
2018-11-12 12:30:30 -0500

    [12.0][MIG] mass_mailing_partner: Migration to v12.0

--- mass_mailing_partner/models/res_partner.py
+++ mass_mailing_partner/models/res_partner.py
@@ -35,8 +35,7 @@ class ResPartner(models.Model):
                     "mailing contact. Email must be assigned."
                 ) % partner.name)
 
-    @api.depends('mass_mailing_contact_ids',
-                 'mass_mailing_contact_ids.opt_out')
+    @api.depends('mass_mailing_contact_ids')
     def _compute_mass_mailing_contacts_count(self):
         contact_data = self.env['mail.mass_mailing.contact'].read_group(
             [('partner_id', 'in', self.ids)], ['partner_id'], ['partner_id'])
